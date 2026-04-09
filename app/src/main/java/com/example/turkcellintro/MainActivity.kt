d2df1935f8b2fd6cc72b5d0e384d3e2f7e7ea44a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-09 19:19:56 +0300

    add myapp

--- app/src/main/java/com/example/turkcellintro/MainActivity.kt
+++ app/src/main/java/com/example/turkcellintro/MainActivity.kt
@@ -4,31 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.turkcellintro.ui.theme.TurkcellIntroTheme
 
-// Class => :ComponentActivity() -> Kalıtım almak
-// Kendi oluşturduğum MainActivity classına Component olma özelliğini ekledim.
 
-// mainActivity: subclass
-// componentActivity: superclass
 class MainActivity : ComponentActivity() {
 
-    // override => kalıtım aldığım classdaki bir methodu değiştirmek/geliştirmek
     override fun onCreate(savedInstanceState: Bundle?) {
-        // super => kalıtım aldığım class
-        // Bu method normalde çalıştığı gibi çalışmaya devam etsin..
         super.onCreate(savedInstanceState)
 
-        // kendi istediğim (bu sayfaya özel) kodlar çalışsın
+        var name: String = "Halit"
+        enableEdgeToEdge()
         setContent {
-
+            Scaffold {
+              paddingValues ->  MyAppStart(Modifier.padding(paddingValues))
+            }
         }
+    }
+}
+
+// Bir fonksiyon eğer UI fonksiyonu ise.. @Composable olması zorunludur..
+@Composable
+fun MyAppStart(modifier: Modifier) {
+    Column( modifier= modifier ) {
+        Text("Merhaba")
+        Text("Merhaba 2")
     }
 }
