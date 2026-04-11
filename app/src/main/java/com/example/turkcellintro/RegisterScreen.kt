ec2e5af8e9b807471ef949e2c0b5f1f850869e19
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-11 10:43:39 +0300

    add navigation package

---
+++ app/src/main/java/com/example/turkcellintro/RegisterScreen.kt
@@ -0,0 +1,9 @@
+package com.example.turkcellintro
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterScreen() {
+    Text("Merhaba")
+}
